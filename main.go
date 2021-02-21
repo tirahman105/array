0fba8998a3092ceb8c4223005db83462ca16a180
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2021-02-21 16:38:15 +0600

    Array basic

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main(){
+
+//array
+
+//var students [3]string
+
+//students[0]="Tareq"
+//students[1]="Anonna"
+//students[2]="Asgor"
+
+//fmt.Println(students [1])
+//fmt.Println (len(students))
+
+//single line array
+
+students:=[...]string{"Tareq", "Anonna", "Asgor", "Rahman"}
+
+fmt.Println(students)
+
+}
+
+
+
+
+
+
+
+
